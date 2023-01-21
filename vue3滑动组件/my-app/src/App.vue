f124a9bcb90f1b689ba9411703cb660bac0ca4b2
songyefeng <1352198650@qq.com>
2023-01-21 23:00:06 +0800

    滑动组件

---
+++ vue3滑动组件/my-app/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+import ImgCheck from "./components/ImgCheck.vue";
+let top = 45;
+
+const getPosition = (x) => {
+  console.log(x, "http");
+};
+</script>
+
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+
+  <div><ImgCheck @getPosition="getPosition" :top="top" /></div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
