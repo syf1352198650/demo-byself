42ee72668a16a90ccb44666e3de9de0ae77780a3
songyefeng <1352198650@qq.com>
2023-01-26 16:25:33 +0800

    vue3自定义修饰符

---
+++ vue3自定义修饰符/midify/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    父{{content  }}
+<MyInput v-model:title.capitalize="content"></MyInput>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import MyInput from "../src/components/MyInput.vue"
+const content=ref('syf')
+
+</script>
+
+<style  scoped>
+
+</style>
---
+++ vue3自定义修饰符/midify/src/components/MyInput.vue
@@ -0,0 +1,32 @@
+<template>
+ <input type="text" :value="props.title" @input="emitValue" />
+</template>
+
+<script setup>
+const emit=defineEmits(['update:title'])
+const props=defineProps({
+  title:{
+    type:String,
+    default:()=>'0'
+  },
+  //对不带参数的v-model绑定,检查对象名称为：modelModifiers
+    //对于带prop参数的v-model绑定，检查对象名称为：prop + modelModifiers
+  titleModifiers:{
+    type:Object,
+    default:()=>{}
+  }
+})
+const emitValue=(e)=>{
+  let value=e.target.value;
+  console.log(value);
+  if(props.titleModifiers.capitalize){
+    value=value.charAt(0).toUpperCase()+value.slice(1);
+  }
+  emit('update:title',value)
+}
+
+</script>
+
+<style scoped>
+
+</style>
